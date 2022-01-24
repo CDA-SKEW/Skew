f4581be1c65dd44481bf56ab81c452848e45e308
kevin.hueri <kevin.hueri@gmail.com>
2022-01-24 16:46:39 +0100

    mise a jour des routes

--- front/src/App.js
+++ front/src/App.js
@@ -1,14 +1,17 @@
-import React from "react";
 import { HashRouter, Route, Routes } from "react-router-dom";
+
+// Import page
 import Home from "./pages/Home";
+import PresentationCandidat from "./pages/PresentationCandidat";
 
 function App() {
   return (
-    <HashRouter>
-      <Routes>
-        <Route path="/" exact element={<Home />} />
-      </Routes>
-    </HashRouter>
+      <HashRouter>
+        <Routes>
+          <Route path="/" index exact element={<Home />} />
+          <Route path="/c" exact element={<PresentationCandidat />} />
+        </Routes>
+      </HashRouter>
   );
 }
 
--- front/src/layouts/Visiteur.js
+++ front/src/layouts/Visiteur.js
@@ -1,9 +1,9 @@
 import { ThemeProvider } from "@mui/material";
-import CssBaseline from "@mui/material/CssBaseline";
-import GlobalStyles from '@mui/material/GlobalStyles';
+// import CssBaseline from "@mui/material/CssBaseline";
+// import GlobalStyles from '@mui/material/GlobalStyles';
 
 import { theme } from "../configs/theme";
-import { style } from '../configs/globalStyle';
+// import { style } from '../configs/globalStyle';
 
 import Navbar from "../components/core/Navbar";
 
@@ -11,8 +11,8 @@ import Navbar from "../components/core/Navbar";
 const MainLayout = ({ children }) => {
   return (
     <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <GlobalStyles styles={{...style}}/>
+      {/* <CssBaseline />
+      <GlobalStyles styles={{...style}}/> */}
       <Navbar />
     </ThemeProvider>
   );
