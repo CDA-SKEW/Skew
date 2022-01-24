a5c2353a9e2d59e084abf4bf06258688fa8fb5f2
wilfried-72 <wilfried.zanni@outlook.fr>
2022-01-24 11:46:28 +0100

    init Architecture front

---
+++ front/src/utils/fonction.js
@@ -0,0 +1,7 @@
+const isValidSiret = (address) => {
+  const reg = /^[0-9]{14}$/;
+  console.log("reg test siret", reg.test(address));
+  return reg.test(address);
+};
+
+export default isValidSiret;
