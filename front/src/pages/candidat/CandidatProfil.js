328b4d2dd7d0b961f78e71cafdbd85a7ca28b401
cda3 <massot.etiennepro@gmail.com>
2022-01-25 10:24:52 +0100

    Add sidebar

--- front/src/pages/candidat/CandidatProfil.js
+++ front/src/pages/candidat/CandidatProfil.js
@@ -1,11 +1,14 @@
 import { Typography } from "@mui/material";
 import { Box } from "@mui/system";
 import React from "react";
+import Sidebar from "components/core/Sidebar"
+
 
 const CandidatProfil = () => {
     return (
         <Box>
             <Typography>Coucou Profil de Bruno</Typography>
+            <Sidebar />
         </Box>
     );
 };
