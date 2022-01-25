90e33eb67dd97bbd65f2d1608f27838cdd0b7e8a
cda2 <soukainataattoumani@yahoo.fr>
2022-01-25 12:23:41 +0100

    add AdminLayout and add AdminPage

--- front/src/App.js
+++ front/src/App.js
@@ -4,14 +4,20 @@ import { HashRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import EmployerDashboard from "pages/employer/EmployerDashboard";
 import PageNotFound from "pages/PageNotFound";
+import AdminPage from "pages/admin/AdminPage";
 
 function App() {
   return (
     <HashRouter>
       <Routes>
         <Route path="/" exact element={<Home />} />
-        <Route path="/Employer/dashboard" exact element={<EmployerDashboard />}/>
+        <Route
+          path="/Employer/dashboard"
+          exact
+          element={<EmployerDashboard />}
+        />
         <Route path="*" element={<PageNotFound />} />
+        <Route path="/admin" exact element={<AdminPage />} />
       </Routes>
     </HashRouter>
   );
