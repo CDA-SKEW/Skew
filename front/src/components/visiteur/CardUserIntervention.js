99b7075ec7bcb3fc5b7dcf1e8541a0657cb87d4a
kevin.hueri <kevin.hueri@gmail.com>
2022-01-25 14:54:10 +0100

    ajout de la banniere collaboration

--- front/src/components/visiteur/CardUserIntervention.js
+++ front/src/components/visiteur/CardUserIntervention.js
@@ -2,23 +2,32 @@ import * as React from 'react';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
-import { CardActionArea } from '@mui/material';
 import Avatar from '@mui/material/Avatar';
 
 export default function CardUserIntervention({ index }) {
     return (
-        <Card>
-            <CardActionArea>
-                <Avatar alt={index.alt} src={index.img} />
-                <CardContent>
-                    <Typography gutterBottom variant="h5" component="div">
-                        {index.prenom} {index.nom}
-                    </Typography>
-                    <Typography variant="body2" color="text.secondary">
-                        {index.message}
-                    </Typography>
-                </CardContent>
-            </CardActionArea>
+        <Card sx={{
+            width: '45%',
+            bgcolor: '#FFD9B8',
+            boxShadow: 'none'
+        }}>
+            <Avatar alt={index.alt} src={index.img} sx={{
+                width: 100,
+                height: 100,
+                m: 'auto'
+            }} />
+            <CardContent>
+                <Typography gutterBottom variant="h5" component="div" sx={{
+                    textAlign: 'center'
+                }}>
+                    {index.prenom} {index.nom}
+                </Typography>
+                <Typography variant="body2" color="text.secondary" sx={{
+                    textAlign: 'justify'
+                }}>
+                    {index.message}
+                </Typography>
+            </CardContent>
         </Card>
     );
 }
--- front/src/components/visiteur/CollaborationCandidat.js
+++ front/src/components/visiteur/CollaborationCandidat.js
@@ -12,15 +12,22 @@ const CollaborationCandidat = () => {
         { prenom: 'Luke', nom: 'Skywalker', img: Avatar1, alt: 'image2', message: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.' },
     ]
     return (
-        <Box>
+        <Box sx={{
+            bgcolor: '#FFD9B8',
+            width: '100%'
+        }}>
             <Typography variant="h5" component="div" sx={{
                 textAlign: 'center'
             }}>
                 Ils ont collaborés avec nous
             </Typography>
-            {UsersList.map((index) => (
-                <CardUserIntervention index={index} />
-            ))}
+            <Box sx={{
+                display: 'flex'
+            }}>
+                {UsersList.map((index) => (
+                    <CardUserIntervention index={index} />
+                ))}
+            </Box>
         </Box>
     );
 };
