7fabffd46a6b25d835356256b5d97fbcc516ad02
cda3 <massot.etiennepro@gmail.com>
2022-01-25 10:08:55 +0100

    Add Page ProfilCandidat

--- front/src/App.js
+++ front/src/App.js
@@ -3,6 +3,7 @@ import { HashRouter, Route, Routes } from "react-router-dom";
 // Import page
 import Home from "./pages/Home";
 import EmployerDashboard from "pages/employer/EmployerDashboard";
+import CandidatProfil from "pages/candidat/CandidatProfil"
 import PageNotFound from "pages/PageNotFound";
 
 function App() {
@@ -14,6 +15,11 @@ function App() {
           path="/Employer/dashboard"
           exact
           element={<EmployerDashboard />}
+        />
+        <Route
+          path="/Candidat/profil"
+          exact
+          element={<CandidatProfil />}
         />
         <Route path="*" element={<PageNotFound />} />
       </Routes>
---
+++ front/src/pages/candidat/CandidatProfil.js
@@ -0,0 +1,13 @@
+import { Typography } from "@mui/material";
+import { Box } from "@mui/system";
+import React from "react";
+
+const CandidatProfil = () => {
+    return (
+        <Box>
+            <Typography>Coucou Profil de Bruno</Typography>
+        </Box>
+    );
+};
+
+export default CandidatProfil;
