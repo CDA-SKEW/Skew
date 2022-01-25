97549f6466ffe34f74b455dca7159ca42f456576
kevin.hueri <kevin.hueri@gmail.com>
2022-01-25 13:46:07 +0100

    init pages de prsentation users

---
+++ front/src/pages/visitor/PresationCandidat.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import VisiteurLayout from 'layouts/VisiteurLayout';
+import { Container } from '@mui/material';
+
+const PresentationCandidat = () => {
+    return (
+        <VisiteurLayout>
+            <Container>
+
+            </Container>
+        </VisiteurLayout>
+    );
+};
+
+export default PresentationCandidat;
---
+++ front/src/pages/visitor/PresentationRecruteur.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import VisiteurLayout from 'layouts/VisiteurLayout';
+import { Container } from '@mui/material';
+
+const PresentationRecruteur = () => {
+    return (
+        <VisiteurLayout>
+            <Container>
+
+            </Container>
+        </VisiteurLayout>
+    );
+};
+
+export default PresentationRecruteur;
