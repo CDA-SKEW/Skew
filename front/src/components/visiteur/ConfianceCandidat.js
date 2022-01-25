6c2d2f4aa13543b38f85f1872e3897b370ec0f6c
kevin.hueri <kevin.hueri@gmail.com>
2022-01-25 15:27:19 +0100

    ajout de la banniere confiance

--- front/src/components/visiteur/ConfianceCandidat.js
+++ front/src/components/visiteur/ConfianceCandidat.js
@@ -1,9 +1,10 @@
 import * as React from 'react';
 import { Box } from '@mui/system';
-import Paper from '@mui/material/Paper';
 import Stack from '@mui/material/Stack';
-import { styled } from '@mui/material/styles';
+import Avatar from '@mui/material/Avatar';
 import ImageListItem from '@mui/material/ImageListItem';
+import Typography from '@mui/material/Typography';
+
 import Logo1 from '../../assets/societesImgs/logo1.jpg';
 import Logo2 from '../../assets/societesImgs/logo2.jpg';
 import Logo3 from '../../assets/societesImgs/logo3.jpg';
@@ -13,40 +14,44 @@ import Logo6 from '../../assets/societesImgs/logo6.jpg';
 import Logo7 from '../../assets/societesImgs/logo7.jpg';
 import Logo8 from '../../assets/societesImgs/logo8.jpg';
 
-const Item = styled(Paper)(({ theme }) => ({
-    ...theme.typography.body2,
-    padding: theme.spacing(1),
-    textAlign: 'center',
-    color: theme.palette.text.secondary,
-}));
-
 export default function DirectionStack() {
 
     const ImgList = [
-        { img: Logo1, alt:'img1', key: "1" },
-        { img: Logo2, alt:'img2', key: "2" },
-        { img: Logo3, alt:'img3', key: "3" },
-        { img: Logo4, alt:'img4', key: "4" },
-        { img: Logo5, alt:'img5', key: "5" },
-        { img: Logo6, alt:'img6', key: "6" },
-        { img: Logo7, alt:'img7', key: "7" },
-        { img: Logo8, alt:'img8', key: "8" },
+        { img: Logo1, alt: 'img1', key: "1" },
+        { img: Logo2, alt: 'img2', key: "2" },
+        { img: Logo3, alt: 'img3', key: "3" },
+        { img: Logo4, alt: 'img4', key: "4" },
+        { img: Logo5, alt: 'img5', key: "5" },
+        { img: Logo6, alt: 'img6', key: "6" },
+        { img: Logo7, alt: 'img7', key: "7" },
+        { img: Logo8, alt: 'img8', key: "8" },
     ]
 
     return (
-        <Box>
+        <Box sx={{
+            my:10
+        }}>
+            <Typography variant="h5" component="div" sx={{
+                textAlign: 'center',
+                my: 5
+            }}>
+                Ils nous font confiance
+            </Typography>
             <Stack direction="row" spacing={2}>
-                {ImgList.map((index) => (
-                    <ImageListItem key={index.key} sx={{
-                        width: "50px",
-                        height: "50px",
-                    }}>
-                        <img
-                            src={index.img}
-                            alt='img'
+                <ImageListItem sx={{
+                    display: 'flex',
+                    py: 5,
+                    borderTop: 1,
+                    borderBottom: 1,
+                }}>
+                    {ImgList.map((index) => (
+                        <Avatar key={index.key} alt={index.alt} src={index.img}
+                            sx={{
+                                mx: 7
+                            }}
                         />
-                    </ImageListItem>
-                ))}
+                    ))}
+                </ImageListItem>
             </Stack>
         </Box>
     );
