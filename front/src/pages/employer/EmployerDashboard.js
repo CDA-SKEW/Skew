06ca1b56304e04d68aae7c213e8be825b9e72c05
wilfried-72 <wilfried.zanni@outlook.fr>
2022-01-25 16:56:43 +0100

    init: component card dashboard employer

--- front/src/pages/employer/EmployerDashboard.js
+++ front/src/pages/employer/EmployerDashboard.js
@@ -1,6 +1,28 @@
 import { Container, CssBaseline, Typography } from "@mui/material";
 import { Box } from "@mui/system";
 import React from "react";
+import CardDashbord from "components/CardDashbord";
+import NotificationsActiveIcon from "@mui/icons-material/NotificationsActive";
+import VisibilityIcon from "@mui/icons-material/Visibility";
+import LibraryBooksIcon from "@mui/icons-material/LibraryBooks";
+
+const arrayDash = [
+  {
+    icon: <LibraryBooksIcon sx={{ width: "45px", height: "45px" }} />,
+    number: 128,
+    text: "Mes offres déposée",
+  },
+  {
+    icon: <VisibilityIcon sx={{ width: "45px", height: "45px" }} />,
+    number: 10,
+    text: "Candidatures reçues",
+  },
+  {
+    icon: <NotificationsActiveIcon sx={{ width: "45px", height: "45px" }} />,
+    number: 5,
+    text: "Notifications",
+  },
+];
 
 const EmployerDashboard = () => {
   return (
@@ -9,72 +31,105 @@ const EmployerDashboard = () => {
       <Container
         sx={{
           bgcolor: "#E5E5E5",
-          mt: 4,
           p: 2,
           height: "100vh",
+          mt: 2,
         }}
       >
         <Box
           sx={{
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
+            bgcolor: "#FFFFFF",
+            height: "auto",
+            borderRadius: 1,
+            my: 4,
           }}
         >
-          <Typography
-            variant="h6"
-            component="h6"
+          <Box
             sx={{
-              px: 1,
-              bgcolor: "#004F98",
-              color: "#FFFFFF",
-              borderRadius: 1,
-              position: "relative",
-              top: 15,
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
             }}
           >
-            Resumé
-          </Typography>
+            <Typography
+              variant="h5"
+              component="h5"
+              sx={{
+                px: 1,
+                bgcolor: "#004F98",
+                color: "#FFFFFF",
+                borderRadius: 1,
+                position: "relative",
+                top: -15,
+              }}
+            >
+              Résumé
+            </Typography>
+          </Box>
+
+          <Box
+            sx={{
+              display: "flex",
+              justifyContent: { xs: "center", md: "space-around" },
+              flexDirection: { xs: "column", md: "row" },
+              alignItems: { xs: "center", md: "none" },
+            }}
+          >
+            {arrayDash.length > 0 &&
+              arrayDash.map((list, index) => (
+                <CardDashbord key={index} list={list} />
+              ))}
+          </Box>
         </Box>
+
+        {/* part2 */}
 
         <Box
           sx={{
             bgcolor: "#FFFFFF",
-            height: "40vh",
+            height: "auto",
             borderRadius: 1,
-          }}
-        />
-
-        <Box
-          sx={{
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
+            mt: 8,
+            mb: 4,
           }}
         >
-          <Typography
-            variant="h6"
-            component="h6"
+          <Box
             sx={{
-              px: 1,
-              bgcolor: "#004F98",
-              color: "#FFFFFF",
-              borderRadius: 1,
-              position: "relative",
-              top: 15,
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
             }}
           >
-            Dernière offres postulées
-          </Typography>
-        </Box>
+            <Typography
+              variant="h5"
+              component="h5"
+              sx={{
+                px: 1,
+                bgcolor: "#004F98",
+                color: "#FFFFFF",
+                borderRadius: 1,
+                position: "relative",
+                top: -15,
+              }}
+            >
+              Dernière offres postulées
+            </Typography>
+          </Box>
 
-        <Box
-          sx={{
-            bgcolor: "#FFFFFF",
-            height: "40vh",
-            borderRadius: 1,
-          }}
-        />
+          <Box
+            sx={{
+              display: "flex",
+              justifyContent: { xs: "center", md: "space-around" },
+              flexDirection: { xs: "column", md: "row" },
+              alignItems: { xs: "center", md: "none" },
+            }}
+          >
+            {arrayDash.length > 0 &&
+              arrayDash.map((list, index) => (
+                <CardDashbord key={index} list={list} />
+              ))}
+          </Box>
+        </Box>
       </Container>
     </React.Fragment>
   );
