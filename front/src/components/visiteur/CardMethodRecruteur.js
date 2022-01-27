6c73f16a5d9833dccdbc2ec6ce6cb2c69884df44
kevin.hueri <kevin.hueri@gmail.com>
2022-01-27 15:53:26 +0100

    travail sur banniere méthodes recruteur

---
+++ front/src/components/visiteur/CardMethodRecruteur.js
@@ -0,0 +1,26 @@
+import { Card, Typography } from '@mui/material'
+import React from 'react'
+import SearchIcon from '@mui/icons-material/Search';
+
+export default function CardMethodRecruteur({ index }) {
+    return (
+        <Card
+            sx={{
+                bgcolor: "#FFD9B8",
+            }}
+        >
+            <SearchIcon
+                fontSize="1000px"
+                sx={{
+                    display: 'block',
+                }}
+            />
+            <Typography variant="h6" color="initial">
+                {index.titre}
+            </Typography>
+            <Typography variant="body1" color="initial">
+                {index.text}
+            </Typography>
+        </Card>
+    )
+}
--- front/src/pages/visitor/PresentationRecruteur.js
+++ front/src/pages/visitor/PresentationRecruteur.js
@@ -4,6 +4,7 @@ import WhyRecruteur from 'components/visiteur/WhyRecruteur';
 import { Container } from '@mui/material';
 import ContactButton from 'components/visiteur/ContactButton';
 import RecrutementBanniere from 'components/visiteur/RecrutementBanniere';
+import MethodsRecruteur from 'components/visiteur/MethodsRecruteur';
 
 export default function PresentationRecruteur() {
     return (
@@ -12,6 +13,7 @@ export default function PresentationRecruteur() {
                 <WhyRecruteur />
                 <ContactButton />
                 <RecrutementBanniere />
+                <MethodsRecruteur />
             </Container>
         </VisiteurLayout>
     );
