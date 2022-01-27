4ac35296659a32f4e1cf2d52b9db0f6bba9390bf
massotetienne <massot.etiennepro@gmail.com>
2022-01-27 11:56:34 +0100

    Add route & page Candidature

---
+++ front/src/pages/candidat/CandidatCandidature.js
@@ -0,0 +1,22 @@
+import { Container, CssBaseline, Typography } from "@mui/material";
+import { Box } from "@mui/system";
+import React from "react";
+
+const CandidatCandidature = () => {
+  return (
+      <Container
+      sx={{
+        bgcolor: "#E5E5E5",
+        p: 2,
+        height: "auto",
+        mt: 2,
+      }}>
+       <Box>
+        <Typography>Candidat Candidature</Typography>
+       </Box>
+      </Container>
+   
+  );
+};
+
+export default CandidatCandidature;
