de80da3c9de676df3b97b4f843aff9cf2c96938b
cda2 <soukainataattoumani@yahoo.fr>
2022-01-27 12:26:11 +0100

    Init: dossier supprimé

--- front/src/pages/admin/AdminPage.js
+++ front/src/pages/admin/AdminPage.js
@@ -2,11 +2,13 @@ import React from "react";
 import AdminLayout from "layouts/AdminLayout";
 import AdminCardUsers from "components/admin/AdminCardUsers";
 import AdminListUsers from "components/admin/AdminListUsers";
+import AdminUsersTable from "./AdminUsersTable";
 
 const AdminPage = () => {
   return (
     <div>
       <AdminLayout>
+        <AdminUsersTable />
         <AdminCardUsers />
         <AdminListUsers />
       </AdminLayout>
