d352bcf6bfd935342ec37a58da049b86bc5279f4
kevin.hueri <kevin.hueri@gmail.com>
2022-01-28 11:30:42 +0100

    partie front de la page contact faite

--- front/src/components/visiteur/FormContact.js
+++ front/src/components/visiteur/FormContact.js
@@ -1,13 +1,79 @@
 import { Typography } from '@mui/material';
 import { Box } from '@mui/system';
 import React from 'react';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
 
-export default function FormContact () {
+export default function FormContact() {
+
+    const InputList = [
+        { key: 1, label: "Nom" },
+        { key: 2, label: "Prenom" },
+        { key: 3, label: "Tel" },
+        { key: 4, label: "Mail" },
+        { key: 5, label: "Sujet" },
+    ]
+
     return (
         <Box>
-            <Typography variant="h2">
+            <Typography
+                variant="h2"
+                sx={{
+                    borderBottom: 2,
+                    mx: 'auto',
+                    mb: 3,
+                    pb: 5,
+                    width: '50%',
+                }}
+            >
                 Contactez-nous
             </Typography>
+            <Box>
+                {InputList.map((index) => (
+                    <TextField
+                        key={index.key}
+                        label={index.label}
+                        color='info'
+                        variant="outlined"
+                        fullWidth
+                        sx={{
+                            display: "block",
+                            width: '40%',
+                            mx: 'auto',
+                            my: 2
+                        }}
+                    />
+                ))}
+                <TextField
+                    label='Message'
+                    color='info'
+                    variant="outlined"
+                    fullWidth
+                    multiline
+                    rows='10'
+                    sx={{
+                        display: "block",
+                        width: '40%',
+                        mx: 'auto',
+                        my: 2
+                    }}
+                />
+                <Button
+                    variant="contained"
+                    color='info'
+                    fullWidth
+                    size='large'
+                    sx={{
+                        display: "block",
+                        width: '40%',
+                        mx: 'auto',
+                        my: 2,
+                        fontSize: '20px'
+                    }}
+                >
+                    Envoyer
+                </Button>
+            </Box>
         </Box>
     );
 };
