1b01e447dbe474b439da24dd56158739fbe97c2b
kevin.hueri <kevin.hueri@gmail.com>
2022-02-03 16:53:01 +0100

    travail sur la partie inscription

--- front/src/components/core/navbar/inscription.js
+++ front/src/components/core/navbar/inscription.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import TextField from '@mui/material/TextField';
 import Box from "@mui/material/Box";
 import Typography from '@mui/material/Typography';
@@ -8,19 +8,51 @@ import Button from '@mui/material/Button';
 
 export default function Inscription() {
 
+    const [prenom, setPrenom] = useState('');
+    const [nom, setNom] = useState('');
+    const [mail, setMail] = useState('');
+    const [pass1, setPass1] = useState('');
+    const [pass2, setPass2] = useState('');
+    const [toggle, setToggle] = useState('');
+
     const [alignment, setAlignment] = React.useState('web');
 
     const handleChange = (event, newAlignment) => {
-        setAlignment(newAlignment);
+        setToggle(newAlignment);
     };
 
     const InscriptionList = [
-        { key: 1, titre: 'Prénom', type: 'text' },
-        { key: 2, titre: 'Nom', type: 'text' },
-        { key: 3, titre: 'Mail', type: 'text' },
-        { key: 4, titre: 'Mot de passe', type: 'password' },
-        { key: 5, titre: 'Confirmation mot de passe', type: 'password' },
+        { key: 1, titre: 'Prénom', type: 'text', name: 'prenom' },
+        { key: 2, titre: 'Nom', type: 'text', name: 'nom' },
+        { key: 3, titre: 'Mail', type: 'text', name: 'mail' },
+        { key: 4, titre: 'Mot de passe', type: 'password', name: 'pass1' },
+        { key: 5, titre: 'Confirmation mot de passe', type: 'password', name: 'pass2' },
     ]
+
+    const handleFormId = (e) => {
+        switch (e.target.name) {
+            case 'prenom':
+                setPrenom(e.target.value)
+                break;
+            case 'nom':
+                setNom(e.target.value)
+                break;
+            case 'mail':
+                setMail(e.target.value)
+                break;
+            case 'pass1':
+                setPass1(e.target.value)
+                break;
+            case 'pass2':
+                setPass2(e.target.value)
+                break;
+            default:
+        }
+    }
+
+    const SubmitFormIdInscription = async () => {
+        console.log('submitFormId', mail, pass1)
+    };
 
     return (
         <Box
@@ -38,9 +70,11 @@ export default function Inscription() {
                 <TextField
                     key={index2.key}
                     label={index2.titre}
+                    name={index2.name}
                     type={index2.type}
                     variant="outlined"
                     fullWidth
+                    onChange={(e) => handleFormId(e)}
                     sx={{
                         my: 1
                     }}
@@ -49,8 +83,9 @@ export default function Inscription() {
             <ToggleButtonGroup
                 color="info"
                 value={alignment}
+                name='toggle'
                 exclusive
-                onChange={handleChange}
+                onChange={(e) => handleChange(e)}
                 fullWidth
                 sx={{
                     my: 1
@@ -62,6 +97,7 @@ export default function Inscription() {
             <Button
                 variant="contained"
                 fullWidth
+                onClick={() => SubmitFormIdInscription()}
                 sx={{
                     bgcolor: '#ABC4FF',
                     fontWeight: 'bold',
