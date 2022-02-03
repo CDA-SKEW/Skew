2e8d829918bc9d6c1b6e96bd461d800286f9f5bf
massotetienne <massot.etiennepro@gmail.com>
2022-02-03 13:53:32 +0100

    pull dev

--- front/src/components/candidat/profil/TableContact.js
+++ front/src/components/candidat/profil/TableContact.js
@@ -32,7 +32,7 @@ export default function TableContact(props) {
           sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
         >
           <TableCell component="th" scope="row" sx={{ display: "none" }}>{index}</TableCell>
-          <TableCell editable="true" >{user.address}<br />{user.zipCode}<br />{user.town}</TableCell>
+          <TableCell  >{user.address}<br />{user.zipCode}<br />{user.town}</TableCell>
           <TableCell >{user.phone}</TableCell>
           <TableCell >{user.mail}</TableCell>
         </TableRow>
