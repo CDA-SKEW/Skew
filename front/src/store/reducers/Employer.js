fc132cfc52fb191f7f29684a2c7d464961197dff
wilfried-72 <wilfried.zanni@outlook.fr>
2022-02-03 16:47:30 +0100

    init: mise en place du store employer avec les methodes post, out profil employer

--- front/src/store/reducers/Employer.js
+++ front/src/store/reducers/Employer.js
@@ -18,17 +18,33 @@ export function EmployerReducer(state = initialState, action) {
   switch (action.type) {
     default:
       return state;
-    case Actions.GET_PROFIL_EMPLOYER:
-      // console.log("GET_PROFIL_EMPLOYER Reducer", action.payload);
-      return {
-        // flash: action.payload.flash,
-        ...state, dataProfilEmployer: action.payload,
-      };
     case Actions.GET_API_SIRET:
       //  console.log("store reducer Api siret", action.payload);
       return {
         // flash: action.payload.flash,
-        ...state, dataSiretApi: action.payload,
+        ...state,
+        dataSiretApi: action.payload,
+      };
+    case Actions.GET_PROFIL_EMPLOYER:
+      // console.log("GET_PROFIL_EMPLOYER Reducer", action.payload);
+      return {
+        // flash: action.payload.flash,
+        ...state,
+        dataProfilEmployer: action.payload,
+      };
+    case Actions.POST_PROFIL_EMPLOYER:
+      // console.log("POST_PROFIL_EMPLOYER Reducer", action.payload);
+      return {
+        // flash: action.payload.flash,
+        ...state,
+        dataProfilEmployer: action.payload,
+      };
+    case Actions.PUT_PROFIL_EMPLOYER:
+      // console.log("PUT_PROFIL_EMPLOYER Reducer", action.payload);
+      return {
+        // flash: action.payload.flash,
+        ...state,
+        dataProfilEmployer: action.payload,
       };
   }
 }
