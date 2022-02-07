491d0d47964c0815d5b8a0b1eac4ca9690aaf871
kevin.hueri <kevin.hueri@gmail.com>
2022-02-07 11:07:46 +0100

    travail sur le moteur de recherche d emploi

--- front/src/components/visiteur/MoteurRechercheOffres.js
+++ front/src/components/visiteur/MoteurRechercheOffres.js
@@ -1,64 +1,112 @@
 import * as React from 'react';
 import Autocomplete from '@mui/material/Autocomplete';
-import TextField from '@mui/material/TextField';
-import Stack from '@mui/material/Stack';
+import SearchIcon from '@mui/icons-material/Search';
+import Paper from "@mui/material/Paper";
+import InputBase from "@mui/material/InputBase";
+import Divider from "@mui/material/Divider";
+import IconButton from "@mui/material/IconButton";
 
 export default function MoteurRechercheOffres() {
     return (
-        <Stack
+
+        <Paper
+            component="form"
             sx={{
-                width: '80%',
-                display: { sx: 'block', md: 'flex' }
+                display: "flex",
+                alignItems: "center",
+                width: 800,
+                height: 75,
+                bgcolor: '#fff',
+                border: 3,
+                mb: 10,
+                mx: "auto",
+                borderRadius: '35px'
             }}
         >
             <Autocomplete
                 multiple
-                options={top100Films.map((option) => option.job)}
+                options={Job.map((option) => option.job)}
                 freeSolo
                 sx={{
-                    width: 250
+                    width: 250,
                 }}
                 renderInput={(params) => (
-                    <TextField
+                    <InputBase
                         {...params}
+                        sx={{
+                            pl: 1
+                        }}
                         placeholder="Job"
                     />
                 )}
             />
+            <Divider
+                variant='fullWidth'
+                sx={{
+                    m: 0.5,
+                }}
+                orientation="vertical"
+            />
             <Autocomplete
                 multiple
-                options={top100Films.map((option) => option.type)}
+                options={Job.map((option) => option.job)}
                 freeSolo
                 sx={{
-                    width: 250
+                    width: 250,
                 }}
                 renderInput={(params) => (
-                    <TextField
+                    <InputBase
                         {...params}
                         placeholder="Type"
                     />
                 )}
             />
+            <Divider
+                variant='fullWidth'
+                sx={{
+                    m: 0.5
+                }}
+                orientation="vertical"
+            />
             <Autocomplete
                 multiple
-                options={top100Films.map((option) => option.lieu)}
+                options={Job.map((option) => option.job)}
                 freeSolo
                 sx={{
-                    width: 250
+                    width: 250,
                 }}
                 renderInput={(params) => (
-                    <TextField
+                    <InputBase
                         {...params}
                         placeholder="Localisation"
                     />
                 )}
             />
-        </Stack>
+            <Divider
+                variant='fullWidth'
+                sx={{
+                    ml: 0.5
+                }}
+                orientation="vertical"
+            />
+            <IconButton
+                type="submit"
+                sx={{
+                    // bgcolor: '#ABC4FF',
+                    borderRadius: "0 35px 35px 0",
+                    height: '100%',
+                    width: 75,
+                }}
+                aria-label="search"
+            >
+                <SearchIcon />
+            </IconButton>
+        </Paper>
     )
 }
 
 
-const top100Films = [
+const Job = [
     { job: 'Developpeur', lieu: 'Lyon', type: 'CDD' },
     { job: 'Maçon', lieu: 'Paris', type: 'CDI' },
     { job: 'Peintre', lieu: 'Marseille', type: 'Interim' },
