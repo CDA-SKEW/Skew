110d945d5073edfebc2ae85c1d5db9d93192da84
cda3 <massot.etiennepro@gmail.com>
2022-02-10 16:52:38 +0100

    fixpage profil

--- front/src/components/candidat/profil/TableExperience.js
+++ front/src/components/candidat/profil/TableExperience.js
@@ -2,11 +2,12 @@ import React, { useEffect, useState } from "react";
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
+import Collapse from "@mui/material/Collapse";
 import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
-import { Button, Stack, TextField, Typography } from "@mui/material";
+import { BottomNavigationAction, Button, Stack, TextField, Typography } from "@mui/material";
 import { Box } from "@mui/system";
 import BorderColorIcon from '@mui/icons-material/BorderColor';
 import AdapterDateFns from '@mui/lab/AdapterDateFns';
@@ -17,6 +18,180 @@ import Grid from '@mui/material/Grid';
 import DeleteIcon from '@mui/icons-material/Delete';
 import CheckCircleOutlineIcon from '@mui/icons-material/CheckCircleOutline';
 import KeyboardReturnIcon from '@mui/icons-material/KeyboardReturn';
+import { useDispatch } from "react-redux";
+import { postFormProfilCandidate, getProfilCandidate } from "store/actions/CandidateActions";
+
+
+
+
+
+function BasicDatePicker() {
+  const [value, setValue] = React.useState(null);
+
+  return (
+    <LocalizationProvider dateAdapter={AdapterDateFns}>
+      <DatePicker
+        label=""
+        value={value}
+        onChange={(newValue) => {
+          setValue(newValue);
+        }}
+        renderInput={(params) => <TextField {...params} />}
+      />
+    </LocalizationProvider>
+  );
+}
+function ModeEdit(props) {
+  const { data } = props
+  const dispatch = useDispatch()
+  const [form, setForm] = useState({})
+
+  const changeForm = (prop) => (event) => {
+    setForm({ ...form, [prop]: event.target.value })
+  }
+
+  const submitForm = () => {
+    dispatch(postFormProfilCandidate(form))
+  }
+
+  console.log('mode edit comp', data)
+
+  return (
+    <TableRow>
+
+      <TableCell align='center' sx={{ minWidth: 200 }} >
+
+        <TextField
+          fullWidth
+          required
+          size="small"
+          id="outlined-required"
+          label="Comp"
+          onChange={() => changeForm('company')}
+          defaultValue={data.company}
+        />
+        <TextField
+          required
+          size="small"
+          id="outlined-required"
+          onChange={() => changeForm('company')}
+          label="Post"
+          defaultValue={data.post}
+        />
+      </TableCell>
+
+      <TableCell align='center' >
+      </TableCell>
+
+      <TableCell align='center' sx={{ minWidth: 200 }} >
+        <TextField
+          fullWidth
+          multiline
+          onChange={() => changeForm('company')}
+          maxRows={4}
+          required
+          size="small"
+          id="outlined-required"
+          label="Description"
+          defaultValue={data.desc}
+        />
+      </TableCell>
+
+      <TableCell align='center' sx={{ minWidth: { xs: 150, sm: 150, md: 150 } }}>
+        <Typography>Start</Typography>
+        <BasicDatePicker />
+
+      </TableCell>
+
+      <TableCell align='center' sx={{ minWidth: { xs: 150, sm: 150, md: 150 } }}>
+        <Typography>End</Typography>
+        <BasicDatePicker />
+      </TableCell>
+
+      <TableCell align='center' sx={{ display: 'flex', flexDirection: 'column' }}>
+        <Button sx={{ color: "green", m: 2 }} onClick={() => submitForm()}>
+          <CheckCircleOutlineIcon />
+        </Button>
+        <Button sx={{ color: "red", m: 2 }}>
+          <KeyboardReturnIcon />
+        </Button>
+      </TableCell>
+    </TableRow>
+
+  );
+};
+
+const CheckModeEdit = (props) => {
+  const { status, row } = props
+  console.log('props mode edit', props)
+  if (status === true) return <ModeEdit data={row} />
+  return <div></div>
+}
+
+function Row(props) {
+  const { row, str } = props
+  const [open, setOpen] = React.useState(false);
+  const [edit, setEdit] = React.useState(false);
+  const dispatch = useDispatch()
+
+  console.log('rowwww', row)
+
+  // const BtnDelete = () => {
+  //   if (open === true) return <Button sx={{ color: "red", }} >
+  //     <DeleteIcon />
+  //   </Button>
+  //   else return;
+  // }
+
+
+  // const BtnDelete = () => {
+  //   if (edit === true) return <Button sx={{ color: "red", }} >
+  //     <DeleteIcon />
+  //   </Button>
+  //   else return;
+  // }
+
+  // const ActionBTN = () => {
+  //   if (edit === true) return <Box><Button onClick={(e) => setOpen(open === true ? false : true)}>
+  //     <BorderColorIcon />
+  //   </Button>
+
+  //     <Button sx={{ color: "red", }} >
+  //       <DeleteIcon />
+  //     </Button>
+  //   </Box>
+
+  // }
+
+
+  return (
+    <React.Fragment>
+      <TableRow sx={{ "&:last-child td,&:last-child th": { border: 0 } }}>
+        <TableCell component="th" scope="row" sx={{ display: "none" }}>0</TableCell>
+
+        <TableCell align='center'>{row.company}</TableCell>
+        <TableCell align='center'>{row.post}</TableCell>
+        <TableCell align='center' sx={{ minWidth: { xs: 400, sm: 400, md: 400 } }}>{row.desc}</TableCell>
+        <TableCell align='center'>{row.start}</TableCell>
+        <TableCell align='center'>{row.end}</TableCell>
+        <Button onClick={(e) => setOpen(open === true ? false : true)}>
+          <BorderColorIcon />
+        </Button>
+
+        <Button sx={{ color: "red", }} >
+          <DeleteIcon />
+        </Button>
+        {/* {ActionBTN()} */}
+
+      </TableRow>
+
+      <CheckModeEdit status={open} row={row} />
+
+    </React.Fragment>
+
+  )
+
+}
 
 export default function TableExperience(props) {
   const { ListExp,
@@ -47,136 +222,20 @@ export default function TableExperience(props) {
     setUseState();
   }, [dataProfilCandidate]);
 
-  // Constante de Condition
 
-  const checkEdit = () => {
-    if (edit === true) return [<ModeText />, <ModeEdit />]
-    else return <ModeText />;
-  }
+
+
+
+
+
   // Constante pour check si le mode edit est actif afficher la colonne action
-  const checkViewAction = () => {
-    if (edit === true) return <TableCell align='center' >Actions </TableCell>
-    else return;
-  }
-  const BtnDelete = () => {
-    if (edit === true) return <Button sx={{ color: "red", }} >
-      <DeleteIcon />
-    </Button>
-    else return;
-  }
 
+  // const checkViewAction = () => {
+  //   if (edit === true) return <TableCell align='center'>Action
+  //   </TableCell>
+  //   else return;
+  // }
 
-
-  function BasicDatePicker() {
-    const [value, setValue] = React.useState(null);
-
-    return (
-      <LocalizationProvider dateAdapter={AdapterDateFns}>
-        <DatePicker
-          label=""
-          value={value}
-          onChange={(newValue) => {
-            setValue(newValue);
-          }}
-          renderInput={(params) => <TextField {...params} />}
-        />
-      </LocalizationProvider>
-    );
-  }
-
-
-
-
-  function ModeText() {
-    return (
-      <TableBody>
-
-        {ListExp.map((exp, index) => (
-          <TableRow key={index} sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
-
-            <TableCell component="th" scope="row" sx={{ display: "none" }}>{index}</TableCell>
-            <TableCell align='center'>{exp.company}</TableCell>
-            <TableCell align='center'>{exp.post}</TableCell>
-            <TableCell align='center' sx={{ minWidth: { xs: '400px', sm: '400px', md: '400px' } }}>{exp.desc}</TableCell>
-            <TableCell align='center'>{exp.start}</TableCell>
-            <TableCell align='center'>{exp.end}</TableCell>
-            {BtnDelete()}
-          </TableRow>
-        ))}
-      </TableBody>
-
-    );
-  }
-  function ModeEdit() {
-    return (
-
-      <TableBody>
-        {ListExp.map((exp) => (
-          <TableRow sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
-            <TableCell align='center' sx={{ minWidth: 140 }} >
-              <TextField
-                fullWidth
-
-                required
-                size="small"
-                id="outlined-required"
-                label="Comp"
-                defaultValue={exp.company}
-              />
-              <TextField
-                required
-                size="small"
-                id="outlined-required"
-                label="Post"
-                defaultValue={exp.post}
-              />
-            </TableCell>
-            <TableCell align='center' >
-              {/* <TextField
-                required
-                size="small"
-                id="outlined-required"
-                label="Post"
-                defaultValue={exp.post}
-              /> */}
-            </TableCell>
-            <TableCell align='center' sx={{ minWidth: 140 }} >
-              <TextField
-                fullWidth
-                multiline
-                maxRows={4}
-                ma
-                required
-                size="small"
-                id="outlined-required"
-                label="Description"
-                defaultValue={exp.desc}
-              />
-            </TableCell>
-            <TableCell align='center' sx={{ minWidth: { xs: 150, sm: 150, md: 150 } }}>
-              <Typography>Start</Typography>
-              <BasicDatePicker />
-
-            </TableCell>
-            <TableCell align='center' sx={{ minWidth: { xs: 150, sm: 150, md: 150 } }}>
-              <Typography>End</Typography>
-              <BasicDatePicker />
-            </TableCell>
-
-            <TableCell align='center' sx={{ display: 'flex', flexDirection: 'column' }}>
-              <Button sx={{ color: "green", m: 2 }} >
-                <CheckCircleOutlineIcon />
-              </Button>
-              <Button sx={{ color: "red", m: 2 }}>
-                < KeyboardReturnIcon />
-              </Button>
-            </TableCell>
-          </TableRow>
-        ))}
-
-      </TableBody>
-    );
-  };
 
 
   return (
@@ -188,18 +247,7 @@ export default function TableExperience(props) {
         my: 4,
       }}
     >
-      <Box
-        sx={{
-          display: "flex",
-          justifyContent: "right",
-          alignItems: "center",
-        }}>
 
-
-        <Button onClick={(e) => setEdit(edit === true ? false : true)}>
-          <BorderColorIcon />
-        </Button>
-      </Box>
       {/* Titre section Formation */}
       <Box
         sx={{
@@ -223,25 +271,41 @@ export default function TableExperience(props) {
           Experience
         </Typography>
       </Box>
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "right",
+          alignItems: "center",
+        }}>
+
+
+        <Button onClick={(e) => setEdit(edit === true ? false : true)}>
+          MODE EDITION
+        </Button>
+      </Box>
 
       <TableContainer sx={{ px: "50px" }} component={Paper}>
         <Table sx={{ width: "100%" }}>
           <TableHead sx={{ bgcolor: "#FF7F50" }}>
             <TableRow>
-              <TableCell align='center'>Company</TableCell>
-              <TableCell align='center' >Job</TableCell>
-              <TableCell align='center'>Description</TableCell>
-              <TableCell align='center'>Start-Year</TableCell>
-              <TableCell align='center'>End-Year</TableCell>
-              {checkViewAction()}
 
+              <TableCell align='left'>Company</TableCell>
+              <TableCell align='left' >Job</TableCell>
+              <TableCell align='left'>Description</TableCell>
+              <TableCell align='left'>Start-Year</TableCell>
+              <TableCell align='left'>End-Year</TableCell>
+              {/* {checkViewAction()} */}
+
+              <TableCell align='left'>Actions</TableCell>
             </TableRow>
           </TableHead>
 
-          {checkEdit()}
+          <TableBody>
+            {ListExp.length > 0 &&
+              ListExp.map((row, index) => <Row key={index} row={row} />)}
 
+          </TableBody>
         </Table>
-
       </TableContainer>
     </Box>
   );
--- front/src/store/actions/CandidateActions.js
+++ front/src/store/actions/CandidateActions.js
@@ -25,50 +25,51 @@ import {
 //     };
 // };
 
+let data = {
+    dataProfilCandidate: {
+        coord:
+        {
+            id: 1,
+            address: '22 rue la shmitrie',
+            zipCode: '44520',
+            town: 'Trou perdu',
+            mail: 'gogo@gadget.com',
+            phone: '0202020202'
+        },
+        exp: [
+            {
+                id: 1, company: 'comp', post: 'dev', desc: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.`,
+                start: '30/02/2018', end: '30/02/2019'
+            },
+            {
+                id: 2, company: 'EDF', post: 'dev-front', desc: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.`,
+                start: '30/08/2016', end: '30/02/2018'
+            }
+        ],
+        skill: ['HTML', 'CSS', 'VUEJS', 'REACTJS', 'FAIRE LE CAFE', 'DU BON', 'TEST'],
+        interest: ['Planche à voile', 'cerveau lent', 'Foot', 'Pillier de comptoir'],
+        certificate: [
+            { id: 1, school: 'Arinfo', title: 'Dev Web', year: 2020, validate: 0 },
+            { id: 2, school: 'Arinfo', title: 'CDA', year: 2022, validate: 0 },
+            { id: 3, school: 'Arinfo', title: 'Faire le café', year: 2023, validate: 0 },
+        ],
+        cv: [
+            { id: 1, title: 'Dev-web' },
+            { id: 2, title: 'CDA' },
+        ]
+    },
+
+    candidatures: {
+        postul: [
+            { id: 1, title: 'Boucher', ets: 'Arinfo', city: 'lambé', date: '31/02/2022', statusCheck: true, statusDist: false, statusQuery: false, statusValid: "Candidature Validé" },
+            { id: 2, title: 'Dev Web', ets: 'Arinfo', city: 'Le Mans', date: '31/02/2022', statusCheck: false, statusDist: true, statusQuery: false },
+            { id: 3, title: 'Tech-lead', ets: 'Arinfo', city: 'Paris 16eme', date: '31/02/2022', statusCheck: false, statusDist: false, statusQuery: true },
+        ]
+    }
+};
+
 // Get profil candidate
 export const getProfilCandidate = () => {
-    const data = {
-        dataProfilCandidate: {
-            coord: [
-                {
-                    address: '22 rue la shmitrie',
-                    zipCode: '44520',
-                    town: 'Trou perdu',
-                    mail: 'gogo@gadget.com',
-                    phone: '0202020202'
-                }
-            ],
-            exp: [
-                {
-                    id: 1, company: 'comp', post: 'dev', desc: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.`,
-                    start: '30/02/2018', end: '30/02/2019'
-                },
-                {
-                    id: 2, company: 'EDF', post: 'dev-front', desc: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.`,
-                    start: '30/08/2016', end: '30/02/2018'
-                }
-            ],
-            skill: ['HTML', 'CSS', 'VUEJS', 'REACTJS', 'FAIRE LE CAFE', 'DU BON', 'TEST'],
-            interest: ['Planche à voile', 'cerveau lent', 'Foot', 'Pillier de comptoir'],
-            certificate: [
-                { id: 1, school: 'Arinfo', title: 'Dev Web', year: 2020, validate: 0 },
-                { id: 2, school: 'Arinfo', title: 'CDA', year: 2022, validate: 0 },
-                { id: 3, school: 'Arinfo', title: 'Faire le café', year: 2023, validate: 0 },
-            ],
-            cv: [
-                { id: 1, title: 'Dev-web' },
-                { id: 2, title: 'CDA' },
-            ]
-        },
-
-        candidatures: {
-            postul: [
-                { id: 1, title: 'Boucher', ets: 'Arinfo', city: 'lambé', date: '31/02/2022', statusCheck: true, statusDist: false, statusQuery: false, statusValid: "Candidature Validé" },
-                { id: 2, title: 'Dev Web', ets: 'Arinfo', city: 'Le Mans', date: '31/02/2022', statusCheck: false, statusDist: true, statusQuery: false },
-                { id: 3, title: 'Tech-lead', ets: 'Arinfo', city: 'Paris 16eme', date: '31/02/2022', statusCheck: false, statusDist: false, statusQuery: true },
-            ]
-        }
-    };
     return (dispatch) => {
         console.log("GET_PROFIL_CANDIDATE action", data);
         dispatch({ type: GET_PROFIL_CANDIDATE, payload: data.dataProfilCandidate });
@@ -77,10 +78,14 @@ export const getProfilCandidate = () => {
 
 
 // Post profil Candidate
-export const postFormProfilCandidate = (data) => {
+export const postFormProfilCandidate = (form) => {
+    console.log('form ezfihjroh', form)
+    // data.dataProfilCandidate.coord = form
+    // data.dataProfilCandidate.exp = formExp
+    // data.dataProfilCandidate.skill = formComp
     return (dispatch) => {
-        // console.log("POST_PROFIL_EMPLOYER action", data);
-        dispatch({ type: POST_PROFIL_CANDIDATE, payload: data });
+        console.log("POST_PROFIL_CANDIDATE action", data);
+        dispatch({ type: POST_PROFIL_CANDIDATE, payload: data.dataProfilCandidate });
     };
 };
 
