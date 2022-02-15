c1cd8a825a39d803b29c8564ebbd131528c0f5a4
kevin.hueri <kevin.hueri@gmail.com>
2022-02-15 14:11:13 +0100

    travail sur la page Home

--- front/src/pages/visitor/Home.js
+++ front/src/pages/visitor/Home.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import VisiteurLayout from 'layouts/VisiteurLayout';
 import CardHome from 'components/visiteur/home/CardHome';
-import WhyHome from 'components/visiteur/WhyHome';
+import WhyHome from 'components/visiteur/home/WhyHome';
 import StatBarHome from 'components/visiteur/StatBarHome';
 
 export default function Home() {
