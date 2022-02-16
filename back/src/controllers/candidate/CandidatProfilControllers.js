21166a3038c0b2ea2dcdb9e0fb18f2e483380f7c
cda3 <massot.etiennepro@gmail.com>
2022-02-16 11:23:22 +0100

    modif

--- back/src/controllers/candidate/CandidatProfilControllers.js
+++ back/src/controllers/candidate/CandidatProfilControllers.js
@@ -15,27 +15,20 @@ class CandidatProfilControllers {
     candidat({ message: "controller Create profil candidat" });
   }
 
-<<<<<<< HEAD:back/src/controllers/CandidatProfilControllers.js
   async updateProfil(req, res) {
-    console.log("controller get Profil candidat");
-    candidat({ message: "controller update profil candidat" });
+    console.log("controller update Profil Candidat");
+    res.send({ message: "controller update profil candidat" });
   }
-=======
-    async updateProfil(req, res) {
-        console.log("controller update Profil Candidat");
-        res.send({ message: "controller update profil candidat" });
-    }
->>>>>>> a0dfa30249886dbffb92414bf15a194c0951f4bb:back/src/controllers/candidate/CandidatProfilControllers.js
 
-    async    deleteProfil(req, res) {
-        console.log("controller del Profil Candidat");
-        res.send({ message: "controller del profil candidat" });
-    }
+  async deleteProfil(req, res) {
+    console.log("controller del Profil Candidat");
+    res.send({ message: "controller del profil candidat" });
+  }
 
- 
+
 
 
 }
 
-module.exports = candidatProfilControllers;
+module.exports = CandidatProfilControllers;
 
