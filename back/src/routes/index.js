1c2e1ac9bf786e9907137cb0d5810a83251409d0
kevin.hueri <kevin.hueri@gmail.com>
2022-02-16 10:46:24 +0100

    route candidatprofil en commentaire

--- back/src/routes/index.js
+++ back/src/routes/index.js
@@ -9,7 +9,7 @@ const AuthControllers = require("../controllers/AuthControllers");
 const UserControllers = require("../controllers/UserControllers");
 const EmployerProfilControllers = require("../controllers/EmployerProfilControllers");
 const UsersControllers = require("../controllers/admin/UsersController");
-const CandidateProfilControllers = ("../controllers/CandidatProfilControllers.js")
+// const CandidateProfilControllers = ("../controllers/CandidatProfilControllers.js")
 
 // Middlewares
 const TestMD = require("../middlewares/Test_md");
@@ -39,15 +39,15 @@ router
   .put(new EmployerProfilControllers().updateProfil);
 
 // CAndidat profil
-router
-  .route("/api/candidat/profil")
-  .get(new CandidatProfilControllers().getProfil)
-  .post(new CandidatProfilControllers().createProfil)
-  .delete(new CandidatProfilControllers().deleteProfil);
+// router
+//   .route("/api/candidat/profil")
+  // .get(new CandidatProfilControllers().getProfil)
+  // .post(new CandidatProfilControllers().createProfil)
+  // .delete(new CandidatProfilControllers().deleteProfil);
 
 router
   .route("/api/candidat/profil/:id")
-  .put(new CandidatProfilControllers().updateProfil);
+  // .put(new CandidatProfilControllers().updateProfil);
 
 // Admin
 router
