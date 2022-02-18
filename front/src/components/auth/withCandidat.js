e57070934599461882cca9c709a161d5d0e1884d
kevin.hueri <kevin.hueri@gmail.com>
2022-02-18 16:53:29 +0100

    travail sur les redirections au login

---
+++ front/src/components/auth/withCandidat.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Navigate } from "react-router-dom";
+import jwt_decode from 'jwt-decode'
+
+export default function withAdmin(Component) {
+    const CandidatRoute = () => {
+        if (!localStorage.getItem("user_token")) return <Navigate to="/" />;
+        else {
+            const token = jwt_decode(localStorage.getItem("user_token"));
+            if (token.isCandidat === 1) return <Component />;
+            else return <Navigate to="/" />;
+        }
+    };
+
+    return CandidatRoute;
+};
