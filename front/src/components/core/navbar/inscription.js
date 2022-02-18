caa413f9168e8f09a30598fc849cf60f8a57bbb0
kevin.hueri <kevin.hueri@gmail.com>
2022-02-18 14:26:20 +0100

    ajout de l outil changement de type password pour l inscription

--- front/src/components/core/navbar/inscription.js
+++ front/src/components/core/navbar/inscription.js
@@ -6,9 +6,51 @@ import Typography from '@mui/material/Typography';
 import ToggleButton from '@mui/material/ToggleButton';
 import ToggleButtonGroup from '@mui/material/ToggleButtonGroup';
 import Button from '@mui/material/Button';
+import InputAdornment from '@mui/material/InputAdornment';
+import IconButton from '@mui/material/IconButton';
+import Visibility from '@mui/icons-material/Visibility';
+import VisibilityOff from '@mui/icons-material/VisibilityOff';
+import FormControl from '@mui/material/FormControl';
+import OutlinedInput from '@mui/material/OutlinedInput';
+import InputLabel from '@mui/material/InputLabel';
 
 import { useDispatch } from "react-redux";
 import { register } from 'store/actions/AuthActions';
+
+function PassInput({ values, handleFormId }) {
+
+    const [showPassword, setShowPassword] = useState(false);
+    const handleClickShowPassword = () => {
+        setShowPassword(!showPassword)
+    };
+
+    const handleMouseDownPassword = (event) => {
+        event.preventDefault();
+    };
+    return (
+        <FormControl sx={{ my: 1 }} variant="outlined" fullWidth>
+            <InputLabel htmlFor="outlined-adornment-password">{values.titre}</InputLabel>
+            <OutlinedInput
+                name={values.name}
+                type={showPassword ? 'text' : 'password'}
+                onChange={(e) => handleFormId(e)}
+                endAdornment={
+                    <InputAdornment position="end">
+                        <IconButton
+                            aria-label="toggle password visibility"
+                            onClick={handleClickShowPassword}
+                            onMouseDown={handleMouseDownPassword}
+                            edge="end"
+                        >
+                            {showPassword ? <VisibilityOff /> : <Visibility />}
+                        </IconButton>
+                    </InputAdornment>
+                }
+                label="Password"
+            />
+        </FormControl>
+    )
+}
 
 export default function Inscription() {
 
@@ -17,14 +59,14 @@ export default function Inscription() {
     const [pass2, setPass2] = useState('');
     const [toggle, setToggle] = useState('');
     const [error, setError] = useState('');
+    const [success, setSuccess] = useState('');
     const [candidat, setCandidat] = useState(0);
     const [recruteur, setRecruteur] = useState(0);
     const dispatch = useDispatch();
 
-    const InscriptionList = [
-        { titre: 'Mail', type: 'text', name: 'mail' },
-        { titre: 'Mot de passe', type: 'password', name: 'pass' },
-        { titre: 'Confirmation mot de passe', type: 'password', name: 'pass2' },
+    const passList = [
+        { titre: 'Mot de passe', name: 'pass' },
+        { titre: 'Confirmer mot de passe', name: 'pass2' },
     ]
 
     const handleChange = (e, newToggle) => {
@@ -58,7 +100,6 @@ export default function Inscription() {
     }
 
     const SubmitFormIdInscription = async (e) => {
-        console.log('SubmitFormIdInscription', mail, pass, pass2, toggle, candidat, recruteur)
         if (mail && toggle && pass && pass2) {
             if (pass === pass2) {
                 await dispatch(register({ mail, pass, candidat, recruteur }));
@@ -67,9 +108,12 @@ export default function Inscription() {
                 setPass2("");
                 setToggle("");
                 setCandidat(0);
-                setRecruteur(0)
+                setRecruteur(0);
+                setSuccess('L\'utilisateur a bien été créé, veuillez valider par mail pour pouvoir vous connecter!');
+                setError('')
             } else {
                 setError('Les mots de passe ne coîncident pas!')
+                setSuccess('')
             }
         } else {
             setError('Entrez tous les champs requis!')
@@ -79,29 +123,23 @@ export default function Inscription() {
 
     return (
         <Box
-            sx={{
-                display: 'block',
-                width: 400,
-                ml: 2,
-            }}>
+            sx={{ display: 'block', width: 400, ml: 2, }}>
             <Typography
                 variant='h4'
             >
                 Inscription
             </Typography>
-            {InscriptionList.map((InscriptionList, index) => (
-                <TextField
-                    key={index}
-                    label={InscriptionList.titre}
-                    name={InscriptionList.name}
-                    type={InscriptionList.type}
-                    variant="outlined"
-                    fullWidth
-                    onChange={(e) => handleFormId(e)}
-                    sx={{
-                        my: 1
-                    }}
-                />
+
+            <TextField
+                label='Mail'
+                name='mail'
+                variant="outlined"
+                fullWidth
+                onChange={(e) => handleFormId(e)}
+                sx={{ my: 1 }} />
+
+            {passList.map((values, index) => (
+                <PassInput key={index} values={values} handleFormId={handleFormId} />
             ))}
             <ToggleButtonGroup
                 color="info"
@@ -116,15 +154,20 @@ export default function Inscription() {
                 <ToggleButton value="candidat">Candidat</ToggleButton>
                 <ToggleButton value="recruteur">Recruteur</ToggleButton>
             </ToggleButtonGroup>
-
             {error.length > 0 &&
                 <Box sx={{ my: 3, color: '#ff0000' }} >
-                    <typography variant='body1' >
+                    <Typography variant='body1' >
                         {error}
-                    </typography>
+                    </Typography>
                 </Box>
             }
-
+            {success.length > 0 &&
+                <Box sx={{ my: 3, color: '#1e90ff' }} >
+                    <Typography variant='body1' >
+                        {success}
+                    </Typography>
+                </Box>
+            }
             <Button
                 variant="contained"
                 fullWidth
