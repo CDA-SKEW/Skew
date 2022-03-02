6c618715b83fb7ff73ecc2bdb9526e5f16952d76
cda2 <soukainataattoumani@yahoo.fr>
2022-03-02 09:25:44 +0100

    Fix: Error pages

--- front/src/pages/candidat/CandidatCandidature.js
+++ front/src/pages/candidat/CandidatCandidature.js
@@ -3,12 +3,8 @@ import React from "react";
 import CandidatureList from "components/candidat/listOffer/CandidatureList";
 import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
-<<<<<<< HEAD
-// import { getProfilCandidate } from "store/actions/CandidateActions";
-=======
 import { getProfilCandidate } from "store/actions/CandidateActions";
 import withCandidat from "components/auth/withCandidat";
->>>>>>> 29e79f3b86879cf4870d9d31aa44e116a6a65ffd
 
 const CandidatCandidature = () => {
   const dispatch = useDispatch()
