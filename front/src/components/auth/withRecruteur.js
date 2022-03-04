2b8ed12a354d25363225c640f582d786572ca9e3
wilfried-72 <wilfried.zanni@outlook.fr>
2022-03-04 15:55:49 +0100

    fix:withrecruteur

--- front/src/components/auth/withRecruteur.js
+++ front/src/components/auth/withRecruteur.js
@@ -4,15 +4,13 @@ import jwt_decode from 'jwt-decode'
 
 export default function withRecruteur(Component) {
     const RecruteurRoute = () => {
-        // if (!localStorage.getItem("user_token")) return <Navigate to="/" />;
-        // else {
-        //     const token = jwt_decode(localStorage.getItem("user_token"));
-        //     if (token.isVerified === 1 && token.isBanned === 0 && token.isRecruteur === 1) return <Component />;
-        //     else return <Navigate to="/" />;
-        // }
-
-
-        return <Component />;
+        if (!localStorage.getItem("user_token")) return <Navigate to="/" />;
+        else {
+            const token = jwt_decode(localStorage.getItem("user_token"));
+            if (token.isVerified === 1 && token.isBanned === 0 && token.isRecruteur === 1) return <Component />;
+            else return <Navigate to="/" />;
+        }
+        // return <Component />;
     };
 
     return RecruteurRoute;
