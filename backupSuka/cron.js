be6f0b4ab85b24be1538c6f711dd5a53dc1077fb
Souka1987 <soukainataattoumani@yahoo.fr>
2022-03-15 20:20:48 +0100

    Init: backup OK

--- backupSuka/cron.js
+++ backupSuka/cron.js
@@ -3,19 +3,15 @@
 
 // Request node-cron & Express
 const cron = require("node-cron");
-const express = require("express");
 const fs = require("fs");
 // Processus enfants
 const { spawn } = require("child_process");
+const moment = require("moment");
+const { exec } = require("child_process");
 
 require("dotenv").config();
 
-app = express(),
-PORT = process.env.PORT || 1870;
-  // Creér le fichier "dumpadmin.sql"
-  // const skew = fs.createWriteStream("dumpadmin.sql");
-
-  /* * * * * * 
+/* * * * * * 
   | | | | | |
   | | | | | day of week
   | | | | month
@@ -24,61 +20,42 @@ PORT = process.env.PORT || 1870;
   | minute
   second ( optional ) */
 
-  // S'exécute toutes les minutes
-  // cron.schedule("* * * * *", function () {
-  //   console.log("Process running every minute");
-  // });
+// Créer un nouveau répertoire avec fs
+// fs.mkdir("../backupSuka/adminBackup", function (err) {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log("New directory successfully created.");
+//   }
+// });
 
-  // var mysqldump = spawn("mysqldump", [
-  //   //   "-u",
-  //     "DB_USER",
-  //     // "-p",
-  //   "DB_PASSWORD",
-  //   "DB_NAME",
-  // ]);
-
-  // mysqldump.stdout
-  //   .pipe(skew)
-  //   .on("finish", function () {
-  //     console.log("Completed");
-  //   })
-  //   .on("error", function (err) {
-  //     console.log(err);
-  //   });
-
-  // Fréquence de sauvegarde comme ici 1 fois par jour
-  cron.schedule(" * * * * * * ", () => {
-    // Générer dynamiquement le nom du fichier avec moment.js
-    const fileName = `${process.env.DATABASE}_${moment().format(
-      "YYYY_MM_DD"
-    )}.sql`;
-    const skew = fs.createWriteStream("dumpadmin.sql");
-    console.log("---------------------");
-    console.log("Running Database Backup Cron Job");
-    const mysqldump = spawn("mysqldump", [
-      "-u",
-      process.env.USERDB,
-      `-p${process.env.PASSWORD}`,
-      process.env.DATABASE,
-    ]);
-
-    mysqldump.stdout
-      .pipe(skew)
-      .on("finish", () => {
-        console.log("DB Backup Completed !");
-      })
-      .on("error", (err) => {
-        console.log(err);
-      });
-
-    cron.schedule("* * * * *", function () {
-      console.log("Process running every minute");
-    });
-  });
-
-// Express run PORT
-app.listen(PORT, function () {
-  console.log(
-    `Ecoute le port ${PORT}, lancé le : ${new Date().toLocaleString()}`
+// Fréquence de sauvegarde comme ici 1 fois par jour
+cron.schedule("* * * * * *", () => {
+  // Générer dynamiquement le nom du fichier avec moment.js
+  const fileName = `${Math.round(Date.now() / 1000)}${
+    process.env.DATABASE
+  }_${moment().format("YYYY_MM_DD")}.dump.sql`;
+  // Ajouter le fichier créé dans un dossier en spécifiant le chemin
+  const skew = fs.createWriteStream(
+    `../backupSuka/adminBackup/${fileName}.gz`,
+    { compressFile: true }
   );
+  console.log("-------------------✈️--------------------");
+  console.log("Running Database Backup Cron Job");
+  // Exécuter
+  const mysqldump = spawn("mysqldump", [
+    "-u",
+    process.env.USERDB,
+    `-p${process.env.PASSWORD}`,
+    process.env.DATABASE,
+  ]);
+
+  mysqldump.stdout
+    .pipe(skew)
+    .on("finish", () => {
+      console.log("Database Backup Completed !  👌");
+    })
+    .on("error", (err) => {
+      console.log(err);
+    });
 });
