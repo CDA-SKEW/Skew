a9576a28079a18bb882ddd216c1450068a49137b
kevin.hueri <kevin.hueri@gmail.com>
2022-03-16 16:54:14 +0100

    update url

--- front/src/utils/url.js
+++ front/src/utils/url.js
@@ -1,5 +1,4 @@
-// const url = 'http://localhost:3033'
-const url = 'http://51.254.115.3:3033'
+const url = 'http://localhost:3033'
 const urlImg= process.env.REACT_APP_URL
 
 module.exports = {url,urlImg}
