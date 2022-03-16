bc03bdbf70fc2adf0f8136a632ac8a8bd5e2fee6
kevin.hueri <kevin.hueri@gmail.com>
2022-03-16 16:47:59 +0100

    update .env

--- front/src/utils/url.js
+++ front/src/utils/url.js
@@ -1,4 +1,5 @@
-const url = 'http://localhost:3033'
+// const url = 'http://localhost:3033'
+const url = 'http://51.254.115.3:3033'
 const urlImg= process.env.REACT_APP_URL
 
 module.exports = {url,urlImg}
