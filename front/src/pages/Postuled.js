1404c00b34c46df95c4ffd59447599c961b1e9ad
cda3 <massot.etiennepro@gmail.com>
2022-03-24 12:22:54 +0100

    add control alreadyPostuled to offer

--- front/src/pages/Postuled.js
+++ front/src/pages/Postuled.js
@@ -4,7 +4,7 @@ import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
 import Select from '@mui/material/Select';
-import { Box, Typography } from '@mui/material';
+import { Box, Chip, Stack, Typography } from '@mui/material';
 import Button from '@mui/material/Button';
 
 // import { useNavigate } from 'react-router-dom';
@@ -33,22 +33,31 @@ export default function Postuled(props) {
 
     const offre = useSelector(state => state.offreVisiteur.offer)
     const Cv = useSelector((state) => state.candidate.userProfil.document)
-    // const ListCandidature = useSelector(state => state.candidate.candidaturesProfil)
     const user = useSelector(state => state.candidate.userProfil.coord)
+    const listCandidatureUser = useSelector(state => state.candidate.candidaturesProfil)
+    // const offreId = { offre_id: offre.offer_id }
+    // const userId = { user_id: user.user_id }
+    // const alreadyPostuled = [{ offreId, userId }]
+    const isPostuled = (arr, value) => {
+        return arr.includes(arr.find(el => {
+            console.log('loop match', el.offer_id, value)
+            return el.offre_id === value
+        }))
+    }
 
-    // const [idCv, setIdCv] = useState("");
-    // const [userId, setUserId] = useState("");
+    console.log("resultat is postuled", isPostuled(listCandidatureUser, offre.offer_id), offre.offer_id);
+    // console.log("result", alreadyPostuled);
+
+
     const [titleCV, setTitleCV] = useState("");
     const [form, setForm] = useState({});
 
+
     useEffect(() => {
-        console.log('offreeeeee', offre.offer_id)
         setForm({ ...form, offre_id: offre.offer_id, user_id: user.id });
     }, [offre])
 
     const handleChangeCv = (prop) => (event) => {
-        console.log('handleChangeCV1', event.target.value)
-        console.log('handleChangeCV2', event.target.name)
         setTitleCV(event.target.value)
         setForm({ ...form, [prop]: event.target.value });
     }
@@ -74,42 +83,46 @@ export default function Postuled(props) {
                     sx={{ mx: 'auto', my: 3, pb: 5, width: '50%', }}>
                     Chez {offre.lastname} {offre.name}
                 </Typography>
-                <Box sx={{ minWidth: 120, maxWidth: 400, mx: 'auto', mt: 10 }}>
-                    <FormControl fullWidth>
-                        <InputLabel id="demo-simple-select-label">CV</InputLabel>
-                        <Select
-                            labelId="demo-simple-select-label"
-                            id="demo-simple-select"
-                            value={titleCV}
-                            label="Cv"
-                            onChange={handleChangeCv('document_id')}
-                        >
-                            {Cv.map((option, index) => {
-                                // console.log('loop select', option)
-                                return (
-                                    <MenuItem key={index} value={option.id_document}>
-                                        {option.title}
-                                    </MenuItem>
-                                )
-                            })}
-                        </Select>
-                    </FormControl>
-
-                    {/* {ListCandidature.length > 0 && (
-                        (Number(ListCandidature[0].offre_id) === Number(offre.offer_id) ?
-                        <Typography textAlign={"center"} color={"red"} p={2}>Vous avez déjà postulé à cette offre </Typography>
-                            :  */}
-                    <Button
-                        variant="contained"
-                        fullWidth
-                        onClick={() => handleClickPostuled()}
-                        sx={{ my: 10 }}
-                    >Envoyer</Button>
-                    {/* )
-                    )} */}
 
 
-                </Box>
+                {
+                    (isPostuled(listCandidatureUser, offre.offer_id) !== true) ?
+                        <Box sx={{ minWidth: 120, maxWidth: 400, mx: 'auto', mt: 10 }}>
+                            <FormControl fullWidth>
+                                <InputLabel id="demo-simple-select-label">CV</InputLabel>
+                                <Select
+                                    labelId="demo-simple-select-label"
+                                    id="demo-simple-select"
+                                    value={titleCV}
+                                    label="Cv"
+                                    onChange={handleChangeCv('document_id')}
+                                >
+                                    {Cv.map((option, index) => {
+                                        // console.log('loop select', option)
+                                        return (
+                                            <MenuItem key={index} value={option.id_document}>
+                                                {option.title}
+                                            </MenuItem>
+                                        )
+                                    })}
+                                </Select>
+                            </FormControl>
+                            <Button
+                                variant="contained"
+                                fullWidth
+                                onClick={() => handleClickPostuled()}
+                                sx={{ my: 10 }}
+                            >Envoyer</Button>
+                        </Box>
+
+                        : <Stack direction="row" spacing={1} display="flex" justifyContent="center" margin={5}>
+
+                            <Chip label="Vous avez déja postuler a cette offre" color="success" />
+                        </Stack>
+                }
+
+
+
             </Box>
         </VisiteurLayout>
     );
